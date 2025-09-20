992c703cafd447ef2bd8227820dccf8f686dcc95
coni.dev <ian.gorski18@gmail.com>
2025-09-20 00:59:58 -0300

    chore: eliminar ruta y referencia a LandingPageAlt para evitar errores de build

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,7 +3,6 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Splash from '../components/Splash';
 import LandingPage from '../components/LandingPage';
-import LandingPageAlt from '../components/LandingPageAlt';
 
 const App = lazy(() => import('../App'));
 
@@ -19,7 +18,6 @@ const AppRoutes = () => (
         <Route path="/" element={<SplashRoute />} />
         <Route path="/app" element={<App />} />
   <Route path="/landing" element={<LandingPage />} />
-  <Route path="/landing-alt" element={<LandingPageAlt />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Suspense>
